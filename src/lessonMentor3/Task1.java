2ecb171eba4c550867053cb13b1cca86c8e8b2dd
Calil13 <ahmadovcalil13@gmail.com>
2025-03-29 20:04:43 +0400

    Array

---
+++ src/lessonMentor3/Task1.java
@@ -0,0 +1,20 @@
+package lessonMentor3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextByte();
+        int[] fioboArray = new int[n];
+        fioboArray[0] = 0;
+        fioboArray[1] = 1;
+
+        for (int i = 2; i < fioboArray.length; i++) {
+            fioboArray[i] = fioboArray[i -1] + fioboArray[i -2];
+        }
+        System.out.println(Arrays.toString(fioboArray));
+    }
+}
