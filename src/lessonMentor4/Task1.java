85a01aebb055d6c888ec59a816aa20c902017143
Calil13 <ahmadovcalil13@gmail.com>
2025-04-07 22:27:39 +0400

    Java OOP

--- src/lessonMentor4/Task1.java
+++ src/lessonMentor4/Task1.java
@@ -1,15 +1,52 @@
 package lessonMentor4;
 
-import java.io.DataOutput;
+import java.util.Scanner;
 
 public class Task1 {
-    //Addition
-    double add(double a, double b) {
-        return a + b;
-    }
 
-    //Subtraction
-    double sub(double a, double b) {
-        return a - b;
+    void calculator(Scanner scanner) {
+
+        System.out.print("Enter first number : ");
+        double num1 = scanner.nextInt();
+
+        System.out.print("Enter second number : ");
+        double num2 = scanner.nextInt();
+
+        System.out.println();
+
+        System.out.println("Choose Operation!");
+        System.out.println("1 - Addition " +
+                "\n2 - Subtraction" +
+                "\n3 - Multiplication " +
+                "\n4 - Division " +
+                "\n5 - Modulus" +
+                "\n6 - Exit!");
+
+        System.out.println();
+
+        System.out.println("Your choice : ");
+        int check = scanner.nextInt();
+
+        switch (check) {
+            case 1:
+                System.out.println("AdditionAddition : " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("Subtraction : " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("Multiplication : " + (num1 * num2));
+                break;
+            case 4:
+                System.out.println("Division : " + (num1 / num2));
+                break;
+            case 5:
+                System.out.println("Modulus : " + (num1 % num2));
+                break;
+            case 6:
+                System.out.println("Exited!");
+            default:
+                System.out.println("Invalid choice! Choose from 1 to 6.");
+        }
     }
 }
--- src/lessonMentor4/TaskCall1.java
+++ src/lessonMentor4/TaskCall1.java
@@ -6,33 +6,7 @@ import java.util.Scanner;
 public class TaskCall1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-
-        System.out.println("1 - Addition " +
-                "\n2 - Subtraction" +
-                "\n3 - Multiplication " +
-                "\n4 - Division " +
-                "\n5 - Modulus");
-
-        switch (num1) {
-
-            case 1:
-                System.out.println(num1 + num2);
-                break;
-            case 2:
-                System.out.println(num1 - num2);
-                break;
-            case 3:
-                System.out.println(num1 * num2);
-                break;
-            case 4:
-                System.out.println(num1 / num2);
-                break;
-            case 5:
-                System.out.println(num1 % num2);
-                break;
-            default:
-        }
+        Task1 task1 = new Task1();
+        task1.calculator(scanner);
     }
 }
