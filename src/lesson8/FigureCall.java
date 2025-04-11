71af3ff13d600d013f2cdf089f8da3e1249cb498
Calil <ahmadovcalil13@gmail.com>
2025-04-11 18:06:42 +0400

    JAVA OOP

--- src/lesson8/FigureCall.java
+++ src/lesson8/FigureCall.java
@@ -2,31 +2,28 @@ package lesson8;
 
 public class FigureCall {
     public static void main(String[] args) {
-        //1 - Circle
         Circle circle = new Circle();
         int radius = 4;
         double perimeterC = circle.Perimete(radius);
         double areaC = circle.area(radius);
         System.out.println("1 - Circle\n" + "Perimeter : " + perimeterC +
-                "\nAre : " + areaC);
+                "\nArea : " + areaC);
 
         System.out.println();
 
-        //2 - Triangle
         Triangle triangle = new Triangle();
         double perimeterT = triangle.perimeter(12,8,5);
         double areaT = triangle.area(10,2);
         System.out.println("2 - Triangle\n" + "Perimeter : " + perimeterT +
-                "\nAre : " + areaT);
+                "\nArea : " + areaT);
 
         System.out.println();
 
-        //3 - Square
         Square square = new Square();
         double side = 10;
         double perimeterS = square.Perimete(side);
         double areaS = square.area(side);
         System.out.println("3 - Square\n" + "Perimeter : " + perimeterS +
-                "\nAre : " + areaS);
+                "\nArea : " + areaS);
     }
 }
