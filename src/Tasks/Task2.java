0ff1c1ec82eb0a747dbb7d71109ad81e06182039
Calil <ahmadovcalil13@gmail.com>
2025-04-16 19:28:22 +0400

    JAVA

--- src/Tasks/Task2.java
+++ src/Tasks/Task2.java
@@ -1,5 +1,23 @@
 package Tasks;
 
 public class Task2 {
-    //iki arrayin var və bunlar tə
+    public static void main(String[] args) {
+        //Input: l1 = [2,4,3], l2 = [5,6,4]
+        //Output: [7,0,8]
+        //Explanation: 342 + 465 = 807.
+
+        int arrNum1 = 0;
+        int arrNum2 = 0;
+        int[] array1 = {0, 0, 3};
+        int[] array2 = {0, 0, 7};
+
+        for (int i = array1.length - 1; i >= 0; i--) {
+            arrNum1 = arrNum1 * 10 + array1[i];
+        }
+
+        for (int j = array2.length - 1; j >= 0; j--) {
+            arrNum2 = arrNum2 * 10 + array2[j];
+        }
+        System.out.println(arrNum2 + arrNum1);
+    }
 }
