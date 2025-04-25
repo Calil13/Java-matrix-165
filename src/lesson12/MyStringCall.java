a994b4d80a31fc22c2c4e4f96986fc376ae98f0b
Calil <ahmadovcalil13@gmail.com>
2025-04-25 19:43:53 +0400

    JAVA

--- src/lesson12/MyStringCall.java
+++ src/lesson12/MyStringCall.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class MyStringCall {
     public static void main(String[] args) {
         MyString myString = new MyString("Calil Ahmadov");
+        System.out.println(MyString.strReverse("Hello"));
         myString.printChar();
     }
 }
