778e8e4a71b8eed042d3d5439ba87bf305bbfaa1
Calil <ahmadovcalil13@gmail.com>
2025-06-06 12:08:43 +0400

    JAVA

---
+++ src/lesson21/Main.java
@@ -0,0 +1,11 @@
+package lesson21;
+
+public class Main {
+    public static void main(String[] args) {
+        MyStringBuffer ms = new MyStringBuffer("Calil");
+        ms.append(" Ahmadov 19");
+
+        System.out.println(ms);
+
+    }
+}
--- src/lesson21/MyStringBuffer.java
+++ src/lesson21/MyStringBuffer.java
@@ -1,19 +1,41 @@
 package lesson21;
 
+import java.util.Arrays;
+
 public class MyStringBuffer {
-    String string;
     private char[] buffer;
+    int length;
 
     public MyStringBuffer(String string) {
-        this.string = string;
-        buffer = new char[100];
+        buffer = new char[string.length() + 10];
+        for (int i = 0; i < string.length(); i++) {
+            buffer[i] = string.charAt(i);
+        }
+        length = string.length();
     }
 
-    public void append(String str){
-        for (int i = 0; i < str.length(); i++) {
-            if (str.length() == 100){
+    public void append(String str) {
+        if (length + str.length() > buffer.length) {
+            char[] newBuffer = new char[length + str.length() * 2];
 
+            for (int i = 0; i < length; i++) {
+                newBuffer[i] = buffer[i];
             }
+            buffer = newBuffer;
         }
+
+        for (int i = 0; i < str.length(); i++) {
+            buffer[length++] = str.charAt(i);
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+
+        for (int i = 0; i < length; i++) {
+            result += buffer[i];
+        }
+        return result;
     }
 }
