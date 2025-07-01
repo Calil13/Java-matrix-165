6a6f0d9bff2e21f9b244d8d88da9f1d39fb87520
Calil <ahmadovcalil13@gmail.com>
2025-07-01 11:04:22 +0400

    JAVA

--- src/lesson24/Main.java
+++ src/lesson24/Main.java
@@ -2,13 +2,23 @@ package lesson24;
 
 public class Main {
     public static void main(String[] args) {
-        MyArrayList<String> text = new MyArrayList<>();
-        text.addValue("Calil");
-        text.addValue(" Ahmadov");
-        text.get();
-        System.out.println();
+//        MyArrayList<String> text = new MyArrayList<>();
+//        text.addValue("Calil");
+//        text.addValue(" Ahmadov");
+//        text.get();
+//        System.out.println();
+//
+//        text.remove(3);
+//        text.get();
 
-        text.remove(3);
-        text.get();
+        MyLinkedList<String> list = new MyLinkedList<>();
+        list.addValue("Calil");
+        list.addValue("Ahmadov");
+        list.get();
+
+        System.out.println("------------");
+
+        list.remove(1);
+        list.get();
     }
 }
--- src/lesson24/MyLinkedList.java
+++ src/lesson24/MyLinkedList.java
@@ -1,4 +1,60 @@
 package lesson24;
 
-//public class MyLinkedList implements MyList{
-//}
+public class MyLinkedList<T> implements MyList<T>{
+    private Node head;
+    private int size;
+
+    public class Node {
+        T data;
+        Node next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    @Override
+    public void addValue(T value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public void remove(int index) {
+        index--;
+
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index!");
+        }
+
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            prev.next = prev.next.next;
+        }
+        size--;
+    }
+
+    @Override
+    public void get() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}
